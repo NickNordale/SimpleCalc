f2edb09fe1497685155a065a785f73a33fa3b77c
Nick-Nordale <nicknordale@gmail.com>
2016-04-04 18:10:25 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Nick Nordale on 4/4/16.
+//  Copyright © 2016 Nick Nordale. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
